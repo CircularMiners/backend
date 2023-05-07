68b0da21417fee934ae30ffd149813a273f5134a
nashid <nashid4722@gmail.com>
2023-05-07 17:43:27 +0300

    added the sreach API for both data rep and requestor

--- src/controllers/sidestream_controller.ts
+++ src/controllers/sidestream_controller.ts
@@ -1,15 +1,56 @@
 import express, { Request, Response } from "express";
 import { SideStreamDTO } from "../entities/sidestream_entity";
-import { insertToDB } from "../services/sidestream";
+import {
+  findMineandSidestream,
+  getAllMineandSidestream,
+  insertToDB,
+} from "../services/sidestream";
 
 const router = express.Router();
 router.use(express.json());
 
-router.post("/miningdata", async (req: Request, res: Response) => {
-  const userdto = req.body as SideStreamDTO;
-  const userRepresentativeDAO = insertToDB(userdto);
-  const response = await userRepresentativeDAO;
-  res.status(201).send(response);
+router.post(
+  "/miningdata/:mineRepId/:mineId",
+  async (req: Request, res: Response) => {
+    const sidestream = req.body as SideStreamDTO;
+    const mineRepId = req.params.mineRepId;
+    const mineId = req.params.mineId;
+    const sidestreamResponse = insertToDB(sidestream, mineRepId, mineId);
+    const response = await sidestreamResponse;
+    res.status(201).send(response);
+  }
+);
+
+router.put(
+  "/miningdata/:mineRepId/:mineId/:sidestreamId",
+  async (req: Request, res: Response) => {
+    const sidestream = req.body as SideStreamDTO;
+    const mineRepId = req.params.mineRepId;
+    const mineId = req.params.mineId;
+    const sidestreamResponse = insertToDB(sidestream, mineRepId, mineId);
+    const response = await sidestreamResponse;
+    res.status(201).send(response);
+  }
+);
+
+//this is for data requestor
+router.get("/miningdata/:oreName", async (req: Request, res: Response) => {
+  const oreName = req.params.oreName;
+  const sidestreams = findMineandSidestream(oreName);
+  const response = await sidestreams;
+  res.status(200).send(response);
 });
+
+//this is for data representative
+router.get(
+  "/miningdata/:mineRepId/:mineId",
+  async (req: Request, res: Response) => {
+    const mineRepId = req.params.mineRepId;
+    const mineId = req.params.mineId;
+    const sidestreams = getAllMineandSidestream(mineRepId, mineId);
+    const response = await sidestreams;
+    res.status(200).send(response);
+  }
+);
 
 export default router;
--- src/services/sidestream.ts
+++ src/services/sidestream.ts
@@ -2,11 +2,17 @@ import {
   MineralCompositionDAO,
   SideStreamDAO,
   SideStreamDTO,
+  SideStreamwithMineDTO,
+  SideStreamwithMineralCompositionDTO,
 } from "../entities/sidestream_entity";
 import { pool } from "../app";
 import { v4 as uuidv4 } from "uuid";
 
-export async function insertToDB(params: SideStreamDTO) {
+export async function insertToDB(
+  params: SideStreamDTO,
+  mineRepId: string,
+  mineId: string
+) {
   const client = await pool.connect();
 
   try {
@@ -15,19 +21,20 @@ export async function insertToDB(params: SideStreamDTO) {
     const insertQuery = `INSERT INTO sidestream 
         (sidestream_id, sidestream_createtime, sidestream_orename,
         sidestream_weight, sidestream_size, mine_representative_id,
-        sidestream_description
-        ) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING 
+        sidestream_description, mine_id
+        ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING 
         sidestream_id, sidestream_createtime, sidestream_orename,
         sidestream_weight, sidestream_size, mine_representative_id,
-        sidestream_description`;
+        sidestream_description, mine_id`;
     const insertValues = [
       uuidv4(),
       new Date(),
       params.oreName,
       params.weight,
       params.size,
-      params.mineRepresentative,
+      mineRepId,
       params.description,
+      mineId,
     ];
 
     const { rows } = await client.query(insertQuery, insertValues);
@@ -60,6 +67,73 @@ export async function insertToDB(params: SideStreamDTO) {
     return sideStream;
   } catch (e) {
     await client.query("ROLLBACK");
+    console.error(e);
+  } finally {
+    client.release();
+  }
+}
+
+export async function findMineandSidestream(oreName: string) {
+  const client = await pool.connect();
+
+  try {
+    const selectQuery = `SELECT sidestream.sidestream_id, sidestream.sidestream_orename, sidestream.sidestream_weight,
+    sidestream.sidestream_size, sidestream.sidestream_description, mine.mine_name, mine.mine_location, mine.mine_description 
+     FROM sidestream LEFT JOIN mine on sidestream.mine_id = mine.mine_id
+     WHERE sidestream_orename = $1`;
+    const insertValues = [oreName];
+
+    const results = await client.query(selectQuery, insertValues);
+
+    const sidestreams: SideStreamwithMineDTO[] = results.rows.map((row) => ({
+      id: row.sidestream_id,
+      oreName: row.sidestream_orename,
+      weight: row.sidestream_weight,
+      size: row.sidestream_size,
+      SidestreamDescription: row.sidestream_description,
+      mineName: row.mine_name,
+      mineLocation: row.mine_location,
+      mineDescription: row.mine_description,
+    }));
+    return sidestreams;
+  } catch (e) {
+    console.error(e);
+  } finally {
+    client.release();
+  }
+}
+
+//newwww
+export async function getAllMineandSidestream(
+  mineRepId: string,
+  mineId: string
+) {
+  const client = await pool.connect();
+
+  try {
+    const selectQuery = `SELECT sidestream.sidestream_id, sidestream.sidestream_orename, sidestream.sidestream_weight,
+    sidestream.sidestream_size, sidestream.sidestream_description, mineralcomposition.mineral_composition_name, 
+    mineralcomposition.mineral_chemical_formula, mineralcomposition.mineral_composition_percentage
+     FROM minerepresentative INNER JOIN sidestream on minerepresentative.mine_representative_id = sidestream.mine_representative_id AND minerepresentative.mine_representative_usertype='representative'
+     LEFT JOIN mineralcomposition on sidestream.sidestream_id = mineralcomposition.sidestream_id
+     WHERE sidestream.mine_representative_id = '${mineRepId}' AND sidestream.mine_id = '${mineId}'`;
+
+    const { rows } = await client.query(selectQuery);
+
+    const sidestreams: SideStreamwithMineralCompositionDTO[] = rows.map(
+      (row) => ({
+        id: row.sidestream_id,
+        oreName: row.sidestream_orename,
+        weight: row.sidestream_weight,
+        size: row.sidestream_size,
+        SidestreamDescription: row.sidestream_description,
+        mineralName: row.mineral_composition_name,
+        mineralFormula: row.mineral_chemical_formula,
+        mineralPercentage: row.mineral_composition_percentage,
+      })
+    );
+    return sidestreams;
+  } catch (e) {
     console.error(e);
   } finally {
     client.release();
